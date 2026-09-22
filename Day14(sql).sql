b3181683b269c36afd5d0a7d79cc3857823a88e5
Riddhima-14 <riddhimasingh1407@gmail.com>
2026-09-22 10:25:25 +0530

    Add files via upload

---
+++ Day14(sql).sql
@@ -0,0 +1,7 @@
+create database Riddhima1;
+use Riddhima1;
+CREATE table student (student_id int, Name varchar(50), Age int, Course varchar(50));
+describe student;
+insert into student (student_id,Name,Age,Course) values (1,"Riddhima",21,"MCA AI"),(2,"Dhruv",26,"MCA AI"),(3,"shivanshu",23,"MCA AI"),(4,"Ravi",28,"MCA AI");
+SELECT * FROM student;
+
